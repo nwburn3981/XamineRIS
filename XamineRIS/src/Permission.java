f9731761a6b49150d1b3cfe7f417fdcb9482cc46
EthanTinsley <79554368+EthanTinsley@users.noreply.github.com>
2021-03-12 15:28:17 -0500

    Update Permission.java

--- XamineRIS/src/Permission.java
+++ XamineRIS/src/Permission.java
@@ -1,5 +1,28 @@
 public class Permission {
 
+	private int accessLvl ;
+	private String name, programName , codeName ;
+	
+	public Permission() {
+		
+	}
+	
+	void setAccessLvl(int level) {
+		accessLvl = level ;
+	}
+	
+	void setName(String firstName, String lastName, String userName) {
+		
+		name = userName + ": " + firstName + " " + lastName ;
+	}
+	
+	void setCodeName(String codeName) {
+		codeName = this.codeName ;
+	}
+	
+	void setProgramName(String program) {
+		programName = program ;
+	}
 
 
 }
