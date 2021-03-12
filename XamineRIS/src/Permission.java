4c6fe465a35c1bd776520c09663ea0a82097e5ed
EthanTinsley <79554368+EthanTinsley@users.noreply.github.com>
2021-03-12 15:33:01 -0500

    Update Permission.java

--- XamineRIS/src/Permission.java
+++ XamineRIS/src/Permission.java
@@ -1,5 +1,12 @@
 public class Permission {
-
+	
+	// This object will serve as our way of ensuring that each job performed within the system corresponds
+	// with the appropriate user IE Access Control. 
+	// it uses an ineger value that determines wich GUI is called by the system so, 0 = no privilages, 1 = Referring Dr, 2 = Reception 
+	// 3 = Technician , 4 = Radiologist 5 = SuperUser 
+	// the Permission object also keeps track of the Users name, program name ex(receptionist), and codename ex(Referring Doctor = RefDr)
+	// Contact me for any questions or concerns! - Ethan 
+	
 	private int accessLvl ;
 	private String name, programName , codeName ;
 	
