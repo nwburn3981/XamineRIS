01039b6c4d2612a75713f890b6de877a6bbc791f
EthanTinsley <79554368+EthanTinsley@users.noreply.github.com>
2021-03-12 14:28:05 -0500

    Update User.java
    Adding getters and setters

--- XamineRIS/src/User.java
+++ XamineRIS/src/User.java
@@ -18,6 +18,83 @@ public class User {
 	
 	}
 	
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public boolean isStaff() {
+		return isStaff;
+	}
+
+	public void setStaff(boolean isStaff) {
+		this.isStaff = isStaff;
+	}
+
+	public boolean isSuperUser() {
+		return isSuperUser;
+	}
+
+	public void setSuperUser(boolean isSuperUser) {
+		this.isSuperUser = isSuperUser;
+	}
+	
+	
+	
+
+}
+
 	
 	
 
