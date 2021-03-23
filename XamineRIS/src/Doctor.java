2fea7ce579e723c23abab8c0a6bcb6d40a2bd0fe
EthanTinsley <79554368+EthanTinsley@users.noreply.github.com>
2021-03-23 17:19:08 -0400

    Update Doctor.java

--- XamineRIS/src/Doctor.java
+++ XamineRIS/src/Doctor.java
@@ -5,15 +5,94 @@ public class Doctor extends User {
 	private boolean isActive, isStaff, isSuperUser ;
 	private Permission userPermission ;
 	
-public Doctor(String firstName, String lastName, String email, String userName) {
+	public Doctor( String firstName, String lastName, String email, String userName) {
 		
 		super(firstName, lastName, email, userName);
 		
-		userPermission.setAccessLvl(1) ;
-		userPermission.setCodeName("SupUser");
-		userPermission.setProgramName("Super User");
-		userPermission.setName(userName, firstName, lastName) ;
 		
-	}//end Doctor
+		userPermission.setAccessLvl(1) ;
+		userPermission.setCodeName("RefDr");
+		userPermission.setProgramName("Referring Doctor");
+		userPermission.setName(userName, firstName, lastName) ;
+	}
 
-}
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public boolean isStaff() {
+		return isStaff;
+	}
+
+	public void setStaff(boolean isStaff) {
+		this.isStaff = isStaff;
+	}
+
+	public boolean isSuperUser() {
+		return isSuperUser;
+	}
+
+	public void setSuperUser(boolean isSuperUser) {
+		this.isSuperUser = isSuperUser;
+	}
+
+	public Permission getUserPermission() {
+		return userPermission;
+	}
+
+	public void setUserPermission(Permission userPermission) {
+		this.userPermission = userPermission;
+	}
+	
