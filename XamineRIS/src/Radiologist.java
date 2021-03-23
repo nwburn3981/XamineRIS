28e7cfa631fe746539e6e37a1a233637cce309a2
mikeygal8 <79479762+mikeygal8@users.noreply.github.com>
2021-03-23 14:06:43 -0400

    Update Radiologist.java

--- XamineRIS/src/Radiologist.java
+++ XamineRIS/src/Radiologist.java
@@ -1,19 +1,47 @@
+import java.security.Permission;
+
 
 public class Radiologist extends User {
 	
-	private String userId, firstName, lastName , password , email , userName; 
+	private String userID, firstName, lastName , password , email , userName; 
 	private boolean isActive, isStaff, isSuperUser ;
 	private Permission userPermission ;
+	private Order order;
+	
 	
 public Radiologist(String firstName, String lastName, String email, String userName) {
 		
 		super(firstName, lastName, email, userName);
 		
 		userPermission.setAccessLvl(4) ;
-		userPermission.setCodeName("SupUser");
+		userPermission.setCodeName("Super User");
 		userPermission.setProgramName("Super User");
 		userPermission.setName(userName, firstName, lastName) ;
 		
 	}//end Radiologist
 
+	
+		
+		public void selectOrder(Order order) {
+			
+		}
+		
+		public void AddReport() {
+			
+		}
+		
+		public void saveReport() {
+			
+		}
+		
+		public String retrieveReport() {
+				
+		}
+		
+		public void SubmitOrder() {
+
+		
+		}
+	
+	
 }
