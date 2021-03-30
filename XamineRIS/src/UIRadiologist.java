a7381497fe8450faabc363281f32d823ee0d0982
EthanTinsley <79554368+EthanTinsley@users.noreply.github.com>
2021-03-30 18:20:50 -0400

    Preparing for use in window builder
    removed requiring an object to run for testing purposes, will add back once GUI is complete - Mikey try this

--- XamineRIS/src/UIRadiologist.java
+++ XamineRIS/src/UIRadiologist.java
@@ -1,31 +1,52 @@
+
+
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import java.awt.BorderLayout;
-import javax.swing.SwingConstants;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.layout.FormSpecs;
-import java.awt.FlowLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import javax.swing.JButton;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JTextField;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
-import javax.swing.JSplitPane;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Panel;
+import javax.swing.JTextField;
+import java.awt.Label;
+import java.awt.Font;
+import javax.swing.JList;
+import java.awt.List;
+import java.awt.TextField;
 import javax.swing.JTextArea;
+import javax.swing.JRadioButton;
+import java.awt.Choice;
 
+//Nick's changes, added static frame for added logout functionality, added User parameter for displaying username, *******
 public class UIRadiologist extends JFrame {
 
-	private JFrame frame;
+	private JPanel contentPane;
+	private JTextField txtsearchPatientFirstName;
+	private JTextField txtsearchPatientLastName;
+	private JTextField txtsearchPatientDoB;
+	private JTextField txtsearchPatientEmail;
+	private JTextField txtviewOrderOrderId;
+	private JTextField txtviewOrderFirstName;
+	private JTextField txtviewOrderDateOfBirth;
+	private JTextField txtviewOrderEmail;
+	private JTextField txtnewPatientEMail;
+	private JTextField txtnewPatientDoB;
+	private JTextField txtnewPatientLastName;
+	private JTextField txtnewPatientGender;
+	private JTextField txtnewPatientPhoneNumber;
+	private JTextField txtnewOrderLastName;
+	private JTextField txtnewOrderDoB;
+	private JTextField txtnewOrderEMail;
+	private JTextField txtnewOrderFirstName;
+	private static UIReferringDoctor frame;
 	private User currentUser;
+
 	/**
 	 * Launch the application.
 	 */
@@ -33,8 +54,8 @@ public class UIRadiologist extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					UIRadiologist window = new UIRadiologist(user);
-					window.frame.setVisible(true);
+					frame = new UIRadiologist();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -42,30 +63,23 @@ public class UIRadiologist extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
-	public UIRadiologist(User user) {
-		currentUser = user;
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 1204, 512);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+	
+	public UIRadiologist() {
+		//currentUser = user;
 		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1204, 512);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 		JPanel statusBar = new JPanel();
 		statusBar.setBackground(UIManager.getColor("CheckBoxMenuItem.selectionBackground"));
 		statusBar.setBounds(0, 0, 1178, 25);
-		frame.getContentPane().add(statusBar);
+		getContentPane().add(statusBar);
 		statusBar.setLayout(null);
 		
-		JLabel versionText = new JLabel("Radiologist Portal");
+		JLabel versionText = new JLabel("Doctor Portal");
 		versionText.setBounds(0, 0, 125, 23);
 		statusBar.add(versionText);
 		versionText.setHorizontalAlignment(SwingConstants.CENTER);
@@ -80,22 +94,73 @@ public class UIRadiologist extends JFrame {
 			public void actionPerformed(ActionEvent click) {
 				
 				UILogin.main(null);
-				frame.dispose();
-				
+				frame.dispose();	
 			}
 		};
 			
 		logoutButton.addActionListener(logoutListener);
 		
-		JLabel nameSpace = new JLabel(currentUser.getFirstName() + " " + currentUser.getLastName());
-		nameSpace.setHorizontalAlignment(SwingConstants.CENTER);
-		nameSpace.setBounds(384, 4, 265, 14);
-		statusBar.add(nameSpace);
-
+		// un-comment this when testing login feature with a user object 
+		//JLabel nameSpace = new JLabel(currentUser.getFirstName() + " " + currentUser.getLastName());
+		//nameSpace.setHorizontalAlignment(SwingConstants.CENTER);
+		//nameSpace.setBounds(384, 4, 265, 14);
+		//statusBar.add(nameSpace);
+		
+		JButton notifyButton = new JButton("Notifications");
+		notifyButton.setBounds(928, 3, 105, 20);
+		statusBar.add(notifyButton);
+		
+		JPanel subActionPanel = new JPanel();
+		subActionPanel.setLayout(null);
+		subActionPanel.setBounds(10, 248, 239, 214);
+		getContentPane().add(subActionPanel);
+		
 		JPanel actionPanel = new JPanel();
-		actionPanel.setBounds(10, 37, 239, 214);
-		frame.getContentPane().add(actionPanel);
+		subActionPanel.add(actionPanel);
+		actionPanel.setBounds(10, 33, 239, 420);
 		actionPanel.setLayout(null);
+		
+		
+
+		JButton newPatient = new JButton("New Patient");
+		newPatient.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		newPatient.setBounds(10, 96, 203, 23);
+		actionPanel.add(newPatient);
+		
+		JButton viewOrdersButton = new JButton("View Open Orders");
+		viewOrdersButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
+		viewOrdersButton.setBounds(10, 62, 203, 23);
+		actionPanel.add(viewOrdersButton);
+		
+		JButton searchPatient = new JButton("Search Patients");
+		searchPatient.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
+		searchPatient.setBounds(10, 28, 203, 23);
+		actionPanel.add(searchPatient);
+		
+		JButton newOrder = new JButton("New Order");
+		newOrder.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
+		newOrder.setBounds(10, 130, 203, 23);
+		actionPanel.add(newOrder);
+		
 	}
 }
 	
