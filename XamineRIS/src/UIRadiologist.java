6db5c179406ddc6c145f70a5ff5c85c07902c22c
mikeygal8 <79479762+mikeygal8@users.noreply.github.com>
2021-04-05 10:41:39 -0400

    Update UIRadiologist.java

--- XamineRIS/src/UIRadiologist.java
+++ XamineRIS/src/UIRadiologist.java
@@ -1,5 +1,157 @@
 
 
+/*import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Panel;
+import javax.swing.JTextField;
+import java.awt.Label;
+import java.awt.Font;
+import javax.swing.JList;
+import java.awt.List;
+import java.awt.TextField;
+import javax.swing.JTextArea;
+import javax.swing.JRadioButton;
+import java.awt.Choice;
+
+//Nick's changes, added static frame for added logout functionality, added User parameter for displaying username, *******
+public class UIRadiologist extends JFrame {
+
+	private JPanel contentPane;
+	private static UIRadiologist frame;
+	//private User currentUser;
+
+	/**
+	 * Launch the application.
+	 
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame = new UIRadiologist();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+	public UIRadiologist() {
+		//currentUser = user;
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1204, 512);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		JPanel statusBar = new JPanel();
+		statusBar.setBackground(UIManager.getColor("CheckBoxMenuItem.selectionBackground"));
+		statusBar.setBounds(0, 0, 1178, 25);
+		getContentPane().add(statusBar);
+		statusBar.setLayout(null);
+		
+		JLabel versionText = new JLabel("Doctor Portal");
+		versionText.setBounds(0, 0, 125, 23);
+		statusBar.add(versionText);
+		versionText.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JButton logoutButton = new JButton("Log out");
+		logoutButton.setBounds(1043, 3, 125, 20);
+		statusBar.add(logoutButton);
+		
+		ActionListener logoutListener = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent click) {
+				
+				UILogin.main(null);
+				frame.dispose();	
+			}
+		};
+			
+		logoutButton.addActionListener(logoutListener);
+		
+		// un-comment this when testing login feature with a user object 
+		//JLabel nameSpace = new JLabel(currentUser.getFirstName() + " " + currentUser.getLastName());
+		//nameSpace.setHorizontalAlignment(SwingConstants.CENTER);
+		//nameSpace.setBounds(384, 4, 265, 14);
+		//statusBar.add(nameSpace);
+		
+		JButton notifyButton = new JButton("Notifications");
+		notifyButton.setBounds(928, 3, 105, 20);
+		statusBar.add(notifyButton);
+		
+		JPanel subActionPanel = new JPanel();
+		subActionPanel.setLayout(null);
+		subActionPanel.setBounds(10, 248, 239, 214);
+		getContentPane().add(subActionPanel);
+		
+		JPanel actionPanel = new JPanel();
+		subActionPanel.add(actionPanel);
+		actionPanel.setBounds(10, 33, 239, 420);
+		actionPanel.setLayout(null);
+		
+		
+
+		JButton newPatient = new JButton("New Patient");
+		newPatient.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		newPatient.setBounds(10, 96, 203, 23);
+		actionPanel.add(newPatient);
+		
+		JButton viewOrdersButton = new JButton("View Open Orders");
+		viewOrdersButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
+		viewOrdersButton.setBounds(10, 62, 203, 23);
+		actionPanel.add(viewOrdersButton);
+		
+		JButton searchPatient = new JButton("Search Patients");
+		searchPatient.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
+		searchPatient.setBounds(10, 28, 203, 23);
+		actionPanel.add(searchPatient);
+		
+		JButton newOrder = new JButton("New Order");
+		newOrder.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
+		newOrder.setBounds(10, 130, 203, 23);
+		actionPanel.add(newOrder);
+		
+	}
+}
+	
+	
+*/
+
+
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
@@ -144,7 +296,34 @@ public class UIRadiologist extends JFrame {
 		newOrder.setBounds(10, 130, 203, 23);
 		actionPanel.add(newOrder);
 		
+		Panel panel = new Panel();
+		panel.setBounds(414, 33, 239, 420);
+		subActionPanel.add(panel);
+		panel.setLayout(null);
+		
+		JButton AddReport = new JButton("Add Report");
+		AddReport.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		AddReport.setBounds(10, 28, 203, 23);
+		panel.add(AddReport);
+		
+		JButton SaveReport = new JButton("Save Report");
+		SaveReport.setBounds(10, 65, 203, 23);
+		panel.add(SaveReport);
+		
+		JButton RetrieveReport = new JButton("Retrieve Report");
+		RetrieveReport.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		RetrieveReport.setBounds(10, 100, 203, 23);
+		panel.add(RetrieveReport);
+		
+		JButton SubmitOrder = new JButton("Submit Order");
+		SubmitOrder.setBounds(10, 135, 203, 23);
+		panel.add(SubmitOrder);
+		
 	}
 }
-	
-	
