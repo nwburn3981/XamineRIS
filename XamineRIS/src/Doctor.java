5a36dc8a9dbc1673c02c699c07b16c57744f1baa
EthanTinsley <79554368+EthanTinsley@users.noreply.github.com>
2021-04-06 19:15:47 -0400

    added methods to interact with SQL

--- XamineRIS/src/Doctor.java
+++ XamineRIS/src/Doctor.java
@@ -36,6 +36,101 @@ public class Doctor extends User {
 		return null ;
 	}
 	
+	
+	public Patient[] returnPatient(String firstName, String lastName, String dateOfBirth, String email) {
+		
+		 Patient[] Patients = new Patient[5] ;
+		 String  DoctorName = this.userName ;
+		 
+		 getConnection() ;
+		
+		// pass this statement to SQL to return the appropriate search criteria 
+		// Select patientID , firstName , lastName , dateOfBirth , email 
+		//	  From patient 
+		//    Where firstName = firstName
+		//    And lastName = lastName 
+		//    AND email = email 
+		//    AND dateOfBirth = dateofBirth 
+		//    AND referringDoctorUserName = DoctorName ;
+		 
+		// insert results into Patient objects here 
+		// can make for loop run as many times as there are sql results up to 5
+		 for (int x = 0 ; x < 5 ; x++) {
+			 // create Patient object with SQL result ( the returned row will correspond with the patient)
+			 Patients[x] = new Patient(SQLfirstName, SQLlastName, SQLEmail );
+			 Patients[x].setDateOfBirth(SQLdateOfBirth ) ;
+			 Patients[x].setPatientId(SQLpatientID) ;
+		 }
+		 
+		return Patients ;
+		
+	}
+	
+	public Order[] returnOrders(String firstName, String lastName, String dateOfBirth, String email) {
+		
+		 Order[] Orders = new Order[5] ;
+		 Patient[] Patients = returnPatient(firstName, lastName, dateOfBirth, email) ;
+		 String  DoctorName = this.userName ;
+		 
+		 getConnection() ;
+		
+		// pass this statement to SQL to return the appropriate search criteria 
+		// Select OrderId, patientID, orderStatus, appointment, visitReason, imagingNeeded
+		//	From imagingOrder
+		//    Where patientID in (Select patientID 
+		//	  	From patient 
+		//    	Where firstName = firstName
+		//   	And lastName = lastName 
+		//   	AND email = email 
+		//    	AND dateOfBirth = dateOfBirth 
+		//    	AND referringDoctorUserName = DoctorName );
+		 	
+		// insert results into Patient objects here 
+		// can make for loop run as many times as there are sql results up to 5
+		 for (int x = 0 ; x < 5 ; x++) {
+			 // create Patient object with SQL result ( the returned row will correspond with the patient)
+			 Orders[x] = new Order(SQLOrderID , Patients[x]);
+			 Orders[x].setImagingOrderStatus(SQLorderStatus);
+			 Orders[x].setApptDay(SQLappointment);
+			 Orders[x].setVisitReason(SQLvisitReason);
+			 Orders[x].setImagingOrder(SQLimagingNeeded);
+			 
+		 }
+		 
+		return Orders ;
+		
+	}
+	
+	public void newPatient(String firstName, String lastName, String dateOfbirth, String email, String gender, String Phone, 
+			boolean latex, boolean xraydye, boolean mridye, boolean asthma, String Notes  ) {
+		
+		int IDnum ;
+		getConnection() ;
+		
+		// SQL need to find the max patient ID and add one to it 
+		// IDnum = select MAX(patientID) from patient ;
+		// IDnum++ ;
+		
+		// SQL Command to insert into Patient table 
+		// Insert into patient values( IDnum , userName, firstName, lastName, email, dateOfbirth, gender , latex, xraydye, mridye , notes , phoneNumber);
+		
+	}
+	
+	public void newOrder( Patient patient , String imagesNeeded , String notes  ) {
+		
+		int IDnum ;
+		getConnection() ;
+		
+		// SQL command needed to find the order with the largest id num 
+		// IDnum = select MAX(orderID) from imagingOrder ;
+		// IDnum++ ;
+		
+		// SQL needed to create a new order.
+		// Insert into imagingorder values( IDnum , patient.getPatientID() , "Open" , null , notes , imagesNeeded , null, null, null, null ) ;
+	}
+
+
+	
 	public String getUserId() {
 		return userId;
 	}
