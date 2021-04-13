462d5ff57e74a762fc913d6bb9fd79b46e3008bf
EthanTinsley <79554368+EthanTinsley@users.noreply.github.com>
2021-04-12 21:52:27 -0400

    Update UIRadiologist.java

--- XamineRIS/src/UIRadiologist.java
+++ XamineRIS/src/UIRadiologist.java
@@ -600,7 +600,7 @@ imageListener = new ActionListener() {
 			   String driver = "com.mysql.cj.jdbc.Driver";
 			   String url = "jdbc:mysql://localhost:3306/xaminedatabase";
 			   String username = "root";
-			   String password = "Restoration2021!";
+			   String password = "";
 			   Class.forName(driver);
 			   
 			   Connection conn = DriverManager.getConnection(url,username,password);
@@ -750,4 +750,3 @@ imageListener = new ActionListener() {
 		statement.executeUpdate();
 		
 	}
-}
