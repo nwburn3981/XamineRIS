f90c4c134b4551ad43cf54cb48ec18352e06a041
EthanTinsley <79554368+EthanTinsley@users.noreply.github.com>
2021-04-12 21:54:26 -0400

    Update UISuperUser.java

--- XamineRIS/src/UISuperUser.java
+++ XamineRIS/src/UISuperUser.java
@@ -132,7 +132,7 @@ public class UISuperUser extends JFrame {
 			   String driver = "com.mysql.cj.jdbc.Driver";
 			   String url = "jdbc:mysql://localhost:3306/xaminedatabase";
 			   String username = "root";
-			   String password = "Et70670!";
+			   String password = "";
 			   Class.forName(driver);
 			   
 			   Connection conn = DriverManager.getConnection(url,username,password);
